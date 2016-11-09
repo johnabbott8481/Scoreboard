e825bdb79b270d01a798325a4a41907fd1d8e3ef
shanemacbride <smacbride@mail.csuchico.edu>
2016-11-09 11:33:55 -0800

    updated Scoreboard header with constructors

--- Scoreboard.h
+++ Scoreboard.h
@@ -12,34 +12,41 @@
 
 class Scoreboard : public IScoreboard
 {
+
   public:
+    Scoreboard();
+    Scoreboard(int players, int periods);
+    ~Scoreboard();
+
   	/*
   	 * void setScore(int,int,int)
   	 *  Sets the score for one competitor for a given period. Out of range
   	 *  values should result in no changes in the scoreboard.
   	 */
-	void setScore(int competitor, int period, int score);
+	  void setScore(int competitor, int period, int score);
 
-	/*
+	  /*
   	 * void getScore(int,int)
   	 *  Returns the score for one competitor for a given period. Out of range
   	 *  values should return INT_MIN
   	 */
-	int getScore(int competitor, int period);
+	 int getScore(int competitor, int period);
 
-	/*
+	  /*
   	 * int getTotalScore(int)
   	 *  Gets the sum of all periods for the given competitor. Out of range
   	 *  values should return 0
   	 */
-	int getTotalScore(int competitor);
+	  int getTotalScore(int competitor);
 
-	/*
+	  /*
   	 * void clearScoreboard()
   	 *  Resets a scoreboard to have scores of 0 for every competitor in
   	 *  every period
   	 */
-	void clearScoreboard();
+	  void clearScoreboard();
 
+  private:
+    double * m_scores;
 };
 #endif /* SCOREBOARD_H_ */
