2950255f1a4180301aa597d709de9244e585bcfb
shanemacbride <smacbride@mail.csuchico.edu>
2016-11-09 11:21:59 -0800

    created Scoreboard class

---
+++ Scoreboard.cpp
@@ -0,0 +1,17 @@
+#include "Scoreboard.h"
+
+void setScore(int competitor, int period, int score)
+{
+}
+
+int getScore(int competitor, int period)
+{
+}
+
+int getTotalScore(int competitor)
+{
+}
+
+void clearScoreboard()
+{
+}
---
+++ Scoreboard.h
@@ -0,0 +1,45 @@
+/*
+ * Scoreboard.h
+ *
+ * Author: CSUDeals Team
+ * Updated: Nov 9, 2016
+ * Description: 
+ *
+ */
+
+#ifndef SCOREBOARD_H_
+#define SCOREBOARD_H_
+
+class Scoreboard : public IScoreboard
+{
+  public:
+  	/*
+  	 * void setScore(int,int,int)
+  	 *  Sets the score for one competitor for a given period. Out of range
+  	 *  values should result in no changes in the scoreboard.
+  	 */
+	void setScore(int competitor, int period, int score);
+
+	/*
+  	 * void getScore(int,int)
+  	 *  Returns the score for one competitor for a given period. Out of range
+  	 *  values should return INT_MIN
+  	 */
+	int getScore(int competitor, int period);
+
+	/*
+  	 * int getTotalScore(int)
+  	 *  Gets the sum of all periods for the given competitor. Out of range
+  	 *  values should return 0
+  	 */
+	int getTotalScore(int competitor);
+
+	/*
+  	 * void clearScoreboard()
+  	 *  Resets a scoreboard to have scores of 0 for every competitor in
+  	 *  every period
+  	 */
+	void clearScoreboard();
+
+};
+#endif /* SCOREBOARD_H_ */
